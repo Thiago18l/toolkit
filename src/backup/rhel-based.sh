a99197f32b9ef63c99b62d9c06f7278d5cd2b776
Thiago Lopes <thiago.lopes.dev@gmail.com>
2022-11-07 19:53:48 -0300

    feat: add scripts to install xtrabackup

---
+++ src/backup/rhel-based.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Script for use only in RHEL based OS
+
+
+BINARY_PATH="wget https://www.percona.com/downloads/XtraBackup/Percona-XtraBackup-2.4.4/\
+binary/redhat/7/x86_64/percona-xtrabackup-24-2.4.4-1.el7.x86_64.rpm"
+
+
+YUM=$(which yum)
+[[ -z ${YUM} ]] && { echo "INFO: Yum not found in this OS"; exit 1; }
+
+
+# Install XTRABACKUP
+wget ${BINARY_PATH}
+
+sudo ${YUM} localinstall percona-xtrabackup-24-2.4.4-1.el7.x86_64.rpm -y
---
+++ src/backup/ubuntu-based.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Script for use only in RHEL based OS
+
+
+BINARY_PATH="wget https://www.percona.com/downloads/XtraBackup/Percona-XtraBackup-2.4.20/\
+binary/debian/stretch/x86_64/percona-xtrabackup-24_2.4.20-1.stretch_amd64.deb"
+
+
+APT=$(which apt)
+[[ -z ${APT} ]] && { echo "INFO: Yum not found in this OS"; exit 1; }
+
+
+# Install XTRABACKUP
+wget ${BINARY_PATH}
+
+sudo ${APT} install percona-xtrabackup-24_2.4.20-1.stretch_amd64.deb -y
